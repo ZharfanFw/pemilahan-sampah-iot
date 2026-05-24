50eb2125c13c8f69186b1ca3c02b60022770c94c
anamaul <maulana140804@gmail.com>
2026-05-24 20:49:42 +0700

    feat: train waste classification model and convert to tfjs for nodejs server

--- model/convert_model.py
+++ model/convert_model.py
@@ -13,9 +13,39 @@ import os
 import sys
 import argparse
 
+# --- Compatibility Monkeypatches for TensorFlowJS & TF 2.16+ in Python 3.12 ---
+# 1. Protobuf compatibility
+os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
+
+# 2. NumPy 1.24+ compatibility (deprecated np.object)
+import numpy as np
+np.object = object
+
+# 3. TF 2.16+ compat.v1.estimator compatibility
+import tensorflow as tf
+import tensorflow.compat.v1 as v1
+DummyExporter = type('DummyExporter', (), {})
+DummyEstimator = type('DummyEstimator', (), {'Exporter': DummyExporter})
+v1.estimator = DummyEstimator
+sys.modules['tensorflow.compat.v1.estimator'] = DummyEstimator
+
+# 4. TF 2.16+ tensorflow.python.training.tracking compatibility
+from types import ModuleType
+tracking_mod = ModuleType('tensorflow.python.training.tracking')
+sys.modules['tensorflow.python.training.tracking'] = tracking_mod
+data_structures_mod = ModuleType('tensorflow.python.training.tracking.data_structures')
+tracking_mod.data_structures = data_structures_mod
+sys.modules['tensorflow.python.training.tracking.data_structures'] = data_structures_mod
+# ------------------------------------------------------------------------------
+
+
+
+
 
 def convert_keras_to_tfjs(model_path: str, output_dir: str) -> None:
     """Convert a Keras .h5 model to TensorFlow.js format."""
+    import tensorflowjs.converters.keras_h5_conversion as khc
+    khc._check_version = lambda h5file: None
     import tensorflowjs as tfjs
     import tensorflow as tf
 
